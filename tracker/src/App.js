e571d2dacd1827437e4527a34ae07fdb3746330e
proencaf <83221947+proencaf@users.noreply.github.com>
2022-01-06 15:01:25 +0000

    rendering no styling

--- tracker/src/App.js
+++ tracker/src/App.js
@@ -1,11 +1,13 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import './App.css'
+import Coin from './component/coin/Coin'
 
 //api.coingecko.com/api/v3/coins/markets?vs_currency=gbp&order=market_cap_desc&per_page=100&page=1&sparkline=false
 
 function App() {
   const [coins, setCoins] = useState([])
+  const [search, setSearch] = useState('')
 
   useEffect(() => {
     axios
@@ -19,11 +21,39 @@ function App() {
       .catch((error) => console.log(error))
   }, [])
 
+  const handleChange = (e) => {
+    setSearch(e.target.value)
+  }
+
+  const filteredCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  )
+
   return (
     <div className='coin-tracker'>
       <div className='coin-search'>
-        <h1 className='coin-text'>Search crypto</h1>
+        <h1 className='coin-text'>Crypto Search</h1>
+        <form>
+          <input
+            type='text'
+            placeholder='search...'
+            className='coin-input'
+            onChange={handleChange}
+          />
+        </form>
       </div>
+      {filteredCoins.map((coin) => {
+        return (
+          <Coin
+            key={coin.id}
+            name={coin.name}
+            image={coin.image}
+            symbol={coin.symbol}
+            volume={coin.market_cap}
+            price={coin.current_price}
+          />
+        )
+      })}
     </div>
   )
 }
